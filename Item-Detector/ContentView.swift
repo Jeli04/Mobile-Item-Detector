0575f9015eaeb373789d6e44db53148292e8fd8f
Jerry <jeli04.2020@gmail.com>
2023-12-24 15:21:13 -0800

    Initial Commit

---
+++ Item-Detector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Item-Detector
+//
+//  Created by Jerry Li on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Item-Detector/Item_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Item_DetectorApp.swift
+//  Item-Detector
+//
+//  Created by Jerry Li on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Item_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
